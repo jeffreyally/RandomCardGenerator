24480c723e75dd3c7a9b2a48744e252e3f1deae7
jeffreyally <jeffreyally@gmail.com>
2022-01-20 20:26:06 +0000

    random number generating ok so far

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".topleftsuit").style.color = "";
+  document.querySelector(".bottomrightsuit").style.color = "";
+  document.querySelector(".middlenumber").innerHTML = `${getRndInteger(2, 10)}`;
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRndInteger(2, 10);
