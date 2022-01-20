b2ecf7e2661437ad3c93335e99da2b20f37c457d
jeffreyally <jeffreyally@gmail.com>
2022-01-20 21:50:24 +0000

    rough draft of project

--- src/app.js
+++ src/app.js
@@ -18,19 +18,20 @@ window.onload = function() {
     ".middlenumber"
   ).innerHTML = `${arrayofValuesforCard[randomIndex]}`;
 };
-if (numforColor == 0) {
-  document.querySelector(".topleftsuit").style.color = "red";
-  document.querySelector(".bottomrightsuit").style.color = "red";
-} else {
-  document.querySelector(".topleftsuit").style.color = "";
-  document.querySelector(".bottomrightsuit").style.color = "";
-}
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var numforColor = randomInt(0, 1);
+
 var numForSuit = randomInt(0, 3);
 var randomIndex = randomInt(0, 12);
 var arrayOfSuitsinHTML = ["&#9830", "&#9827", "&#9824", "&#9829"];
 var arrayofValuesforCard = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+
+if (randomInt(1, 2) == 1) {
+  document.querySelector(".topleftsuit").style.color = "red";
+  document.querySelector(".bottomrightsuit").style.color = "red";
+} else {
+  document.querySelector(".topleftsuit").style.color = "black";
+  document.querySelector(".bottomrightsuit").style.color = "black";
+}
